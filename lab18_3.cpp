bc70c331502a9c8be7c02376d365a1439fe42f04
godmonera <borwonpak_du@cmu.ac.th>
2023-02-14 16:27:41 +0700

    finished

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -21,4 +21,44 @@ int main(){
 	return 0;
 }
 
+vector<int> randomVector(int x){
+
+vector<int> v;
+for(unsigned int i=0; i<x ;i++){
+
+    v.push_back(rand()%10);
+
+}
+
+	return v;
+}
+
+
+void showVector(vector<int> x){
+
+	cout << "[" ;
+
+  for(unsigned int i=0; i< x.size() ; i++){
+
+    cout << x[i];
+	if( i == x.size()-1) cout << "]";
+	else cout << " ";
+
+  }
+
+
+}
+
+
+int dotProduct(vector<int> x,vector<int> y){
+
+int sum =0 ;
+for(unsigned int i=0 ; i< x.size() ; i++){
+
+sum += x[i]*y[i]; 
+
+}
+return sum;
+}
+
 //Write Definition of randomVector() showVector() and dotProduct() here
